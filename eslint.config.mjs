e42ce4ba3f618e6f3736b224dade93d858cbcc2a
Ethan Krich <ektechworld@gmail.com>
2025-07-10 18:43:52 -0700

    fix: remove unused import

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,3 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
-
 import reactNative from "@react-native/eslint-config";
 import react from "eslint-plugin-react";
 
