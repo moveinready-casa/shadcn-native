abb6f2a052e8b189304a46ebe74ead8b03127745
Ethan Krich <ektechworld@gmail.com>
2025-07-10 19:22:26 -0700

    fix: add tseslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,40 @@
-import reactNative from "@react-native/eslint-config";
+import reactNative from "@react-native/eslint-plugin";
 import react from "eslint-plugin-react";
+import globals from "globals";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
 
 /**
  * @type {import("eslint").Linter.Config[]}
  */
 const eslintConfig = [
   {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "coverage/**",
+      "*.tsbuildinfo",
+      "next-env.d.ts",
+      ".turbo/**",
+      "storybook-static/**",
+      ".stories/**",
+      "public/_pagefind",
+    ],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "prefer-const": "warn",
+      "@typescript-eslint/no-require-imports": "off",
+    },
+  },
+  {
     files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
+    ignores: ["app/registry"],
     plugins: {
       react,
     },
@@ -26,8 +54,11 @@ const eslintConfig = [
     },
   },
   {
-    ...reactNative,
-    ignores: ["node_modules", "!/app/registry"],
+    files: ["app/registry/**/*.{js,jsx,mjs,cjs,ts,tsx}"],
+    plugins: {
+      reactNative,
+    },
+    ignores: ["!/app/registry"],
   },
 ];
 
