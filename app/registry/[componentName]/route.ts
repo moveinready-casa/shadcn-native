02cda1d263e3235a88082ee8e3121ca50d6baf69
Ethan Krich <ektechworld@gmail.com>
2025-08-20 21:00:00 -0700

    chore: add registry

---
+++ app/registry/[componentName]/route.ts
@@ -0,0 +1,44 @@
+import {readFileSync} from "fs";
+import {join} from "path";
+import {NextResponse} from "next/server";
+
+export async function GET(
+  request: Request,
+  {params}: {params: Promise<{componentName: string}>},
+) {
+  const routeParams = await params;
+
+  try {
+    const filePath = join(
+      process.cwd(),
+      "public",
+      "r",
+      `${routeParams.componentName}.json`,
+    );
+    const fileContent = readFileSync(filePath, "utf-8");
+    const componentData = JSON.parse(fileContent);
+
+    return NextResponse.json(componentData);
+  } catch (error) {
+    try {
+      const legacyFilePath = join(
+        process.cwd(),
+        "registry",
+        "ui",
+        `${routeParams.componentName}.json`,
+      );
+      const legacyFileContent = readFileSync(legacyFilePath, "utf-8");
+      const legacyComponentData = JSON.parse(legacyFileContent);
+      console.error(error);
+
+      return NextResponse.json(legacyComponentData);
+    } catch (legacyError) {
+      return new NextResponse(
+        JSON.stringify({
+          error: `Component ${routeParams.componentName} not found, ${legacyError}`,
+        }),
+        {status: 404, headers: {"content-type": "application/json"}},
+      );
+    }
+  }
+}
